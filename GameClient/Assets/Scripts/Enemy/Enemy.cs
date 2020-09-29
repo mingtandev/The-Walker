9f3e6eff4a3cf0e3b7d94664a72aee29835780b9
Okabe Dev <tanlalan123@gmail.com>
2020-09-29 11:09:33 +0700

    make AI zombie , attack , move , idle

--- GameClient/Assets/Scripts/Enemy/Enemy.cs
+++ GameClient/Assets/Scripts/Enemy/Enemy.cs
@@ -15,11 +15,14 @@ public class Enemy : MonoBehaviour
     enum Status
     {
         Idle,
-        Chase
+        Chase,
+        Attack
     }
 
     public int heath;
+    public GameObject attackPoint;
     public NavMeshAgent agent;
+    bool isDeath;
     [SerializeField]
     TypeOfZombie ZombieType;
 
@@ -48,36 +51,59 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (heath <= 0)
+        if (!isDeath)
         {
-            StartCoroutine(UnActive());
-            anim.Play("Die");
+            if (heath <= 0)
+            {
+                StartCoroutine(DieAction());
+            }
 
+            Attack();
         }
     }
 
-    IEnumerator UnActive()
+
+    void Attack()
     {
-        Die();
+        // Debug.DrawRay(attackPoint.transform.position,-direc.normalized,Color.yellow);
+        Collider[] obj = Physics.OverlapSphere(attackPoint.transform.position, 1f, LayerMask.GetMask("PlayerLayer"));
+        if (obj.Length > 0)
+        {
+            if (obj[0].tag == "Player")
+            {
+                WalkerStatus = Status.Attack;
+                anim.SetBool("Attack", true);
+            }
+        }
+        else
+        {
+            anim.SetBool("Attack", false);
+        }
+    }
+
+    IEnumerator DieAction()
+    {
+        anim.Play("Die");
+        isDeath = true;
+
+        StopCoroutine(Couroutine_PathFiding);
+        GetComponent<NavMeshAgent>().enabled = false;  //stop path finding
+
         yield return new WaitForSeconds(2f);
+
         this.enabled = false;
         GetComponent<CapsuleCollider>().enabled = false;
         GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionY;
-    }
 
-    void Die()
-    {
-        StopCoroutine(Couroutine_PathFiding);
-        GetComponent<NavMeshAgent>().enabled = false;
     }
 
     IEnumerator PathFinding()
     {
-        agent.SetDestination(transform.position);
         float dis = Vector3.Distance(transform.position, player.transform.position);
         switch (WalkerStatus)
         {
             case Status.Idle:
+                agent.SetDestination(transform.position);
                 if (dis < DistanceToChase)
                 {
                     WalkerStatus = Status.Chase;
@@ -90,6 +116,13 @@ public class Enemy : MonoBehaviour
                 {
                     WalkerStatus = Status.Idle;
                     anim.SetBool("Chasing", false);
+                }
+                break;
+            case Status.Attack:
+                agent.SetDestination(transform.position);
+                if (dis < DistanceToChase)   
+                {
+                    WalkerStatus = Status.Chase;   //Tai5 enum attack , tức là lúc attack phải set des tại chỗ để tấn công , sau đó set chase vì nhân vật chắc chắc k di chuyen quá xa
                 }
                 break;
         }
