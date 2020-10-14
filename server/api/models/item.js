240024ff3b3b4f173f9df044dbeb87fc2c65641d
Nguyen-Quoc-Thai <ct1.nquocthai@gmail.com>
2020-10-14 20:03:19 +0700

    Solve conflict

---
+++ server/api/models/item.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+const slug = require('mongoose-slug-generator')
+
+const itemScheme = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    slugName: {
+        type: String,
+        slug: 'name',
+        unique: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    thumbnail: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    sale: {
+        type: Number,
+        default: 0
+    },
+    saleExpiresTime: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+// Add plugins
+mongoose.plugin(slug)
+itemScheme.set('timestamps', true)
+
+module.exports = mongoose.model('Item', itemScheme)
---
+++ server/app.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+require('dotenv').config()
+
+// DB
+const connectDB = require('./api/config/db')
+
+// Routes
+const userRoutes = require('./api/routes/users')
+const itemRoutes = require('./api/routes/items')
+const giffcodeRoutes = require('./api/routes/giffcodes')
+const blogRoutes = require('./api/routes/blogs')
+const rollupRoutes = require('./api/routes/rollups')
+const userItemRoutes = require('./api/routes/userItems')
+
+const app = express()
+
+// Connect DB
+connectDB()
+
+// Middleware
+app.use(morgan('dev'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(cors())
+
+// Routes
+app.use('/uploads', express.static('./uploads'))
+app.use('/users', userRoutes)
+app.use('/items', itemRoutes)
+app.use('/giffcodes', giffcodeRoutes)
+app.use('/blogs', blogRoutes)
+app.use('/rolls', rollupRoutes)
+app.use('/user-items', userItemRoutes)
+
+// Handling server errors
+app.use((req, res, next) => {
+    const error = new Error('Not found!')
+    error.status = 404
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        msg: error.message
+    })
+})
+
+module.exports = app
