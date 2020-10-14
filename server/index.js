f7f0ab16cf1bb1899b23c3175c4004ef015fd4d2
Nguyen-Quoc-Thai <ct1.nquocthai@gmail.com>
2020-10-14 20:01:52 +0700

    Solve conflict

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-
-const app = require('./app')
-
-const port = process.env.PORT
-
-const server = http.createServer(app)
-
-server.listen(port, () => `Server is listening at port: ${port}`)
