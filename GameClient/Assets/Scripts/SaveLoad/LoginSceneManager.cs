35b38ac2def33db963459d857c5532358702930f
DESKTOP-KCQ2H6B\tanla <tanlalan123@gmail.com>
2020-11-29 11:16:35 +0700

    Create UI login ,  call API login

---
+++ GameClient/Assets/Scripts/SaveLoad/LoginSceneManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginSceneManager : MonoSingleton<LoginSceneManager>
+{
+    public static Token token;
+    public static PlayerData playerData;
+    
+    
+}
